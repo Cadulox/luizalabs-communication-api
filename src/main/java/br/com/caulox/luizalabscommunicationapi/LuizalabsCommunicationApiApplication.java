fd9a99c00cd6392aaf102dcc67a706c63cea53d7
Cadulox <cadulox@gmail.com>
2021-08-17 10:41:20 -0300

    Project created

---
+++ src/main/java/br/com/caulox/luizalabscommunicationapi/LuizalabsCommunicationApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.caulox.luizalabscommunicationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizalabsCommunicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizalabsCommunicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caulox/luizalabscommunicationapi/LuizalabsCommunicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caulox.luizalabscommunicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuizalabsCommunicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
